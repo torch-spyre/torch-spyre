b1ae8ccc9a5db9f6ee4bcc927a6182742ba86fc3
Olivier Tardieu <tardieu@us.ibm.com>
2025-11-11 21:42:52 +0000

    Add copyright headers to python source code

--- codegen/gen.py
+++ codegen/gen.py
@@ -1,4 +1,17 @@
-# Copyright IBM Corp. 2025
+# Copyright 2025 The Torch-Spyre Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 PyTorch has 3000+ registered functions in aten, and some of these require registration to extend dispatcher.
 There are three main ways to handle all wrapper functions depending on the backend support:
