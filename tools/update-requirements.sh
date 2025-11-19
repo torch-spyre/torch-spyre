4d401ae6fd9c5e8fc81670d00999a2ecc7ecf026
Olivier Tardieu <tardieu@us.ibm.com>
2025-11-19 14:22:53 +0000

    Eliminate all.txt requirements
    Signed-off-by: Olivier Tardieu <tardieu@us.ibm.com>

--- tools/update-requirements.sh
+++ tools/update-requirements.sh
@@ -7,6 +7,4 @@ cd ${ROOT_DIR}
 uv pip compile pyproject.toml --emit-index-url > requirements/run.txt
 uv pip compile pyproject.toml --emit-index-url --extra lint --no-emit-package torch > requirements/lint.txt
 uv pip compile pyproject.toml --emit-index-url --extra lint --extra build > requirements/build.txt
-uv pip compile pyproject.toml --emit-index-url --extra lint --extra build --extra test > requirements/all.txt
-uv pip compile pyproject.toml --emit-index-url --extra lint --extra build --extra test --no-emit-package torch > requirements/dev.txt
-
+uv pip compile pyproject.toml --emit-index-url --extra lint --extra build --extra test > requirements/dev.txt
