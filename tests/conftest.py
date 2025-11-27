12cad01e242e6570a30fd682b02bf6a6189e8ec8
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2025-11-27 06:49:04 +0000

    add reasons for skip and xfail

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,17 +37,32 @@ def pytest_collection_modifyitems(config, items):
     with open(yaml_path, "r") as f:
         data = yaml.safe_load(f) or {}
 
-    skip_files = data.get("skip", [])
-    skip_paths = [pathlib.Path(p).resolve() for p in skip_files]
-    skip_marker = pytest.mark.skip(reason="marked skip in skip_files.yaml")
+    skip_markers = {}
+    xfail_markers = {}
 
-    xfail_files = data.get("xfail", [])
-    xfail_paths = [pathlib.Path(p).resolve() for p in xfail_files]
-    xfail_marker = pytest.mark.xfail(reason="marked xfail in skip_files.yaml")
+    reasons = data.get("skip", [])
+    for reason in reasons:
+        marker = pytest.mark.skip(reason=f"marked skip in skip_files.yaml: {reason}")
+        skip_files = data["skip"][str(reason)]
+        print(reason, skip_files)
+        skip_paths = [pathlib.Path(p).resolve() for p in skip_files]
+        for p in skip_paths:
+            print("SKIP:", p)
+            skip_markers[p] = marker
+
+    reasons = data.get("xfail", [])
+    for reason in reasons:
+        marker = pytest.mark.xfail(reason=f"marked xfail in skip_files.yaml: {reason}")
+        xfail_files = data.get(reason, [])
+        xfail_paths = [pathlib.Path(p).resolve() for p in xfail_files]
+        for p in xfail_paths:
+            print("SKIP:", p)
+            xfail_markers[p] = marker
 
     for item in items:
         test_file = pathlib.Path(item.fspath).resolve()
-        if test_file in skip_paths:
-            item.add_marker(skip_marker)
-        if test_file in xfail_paths:
-            item.add_marker(xfail_marker)
+        print("T:", test_file)
+        if test_file in skip_markers:
+            item.add_marker(skip_markers[test_file])
+        if test_file in xfail_markers:
+            item.add_marker(xfail_markers[test_file])
