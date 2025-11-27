e7aaeb99f142b0e20d28cf68a7dd98f8616e9e27
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2025-11-27 06:48:27 +0000

    add --report option

--- tests/test_models_ops.py
+++ tests/test_models_ops.py
@@ -8,8 +8,10 @@ def main():
     parser = argparse.ArgumentParser(description="driver for running test cases derived from models") 
     parser.add_argument("--opname", help="torch operation name to be tested")
     parser.add_argument("--ignore_skip_files", action="store_true", help="ignore model/skip_files.yaml")
+    parser.add_argument("--report", type=str, help="Show extra test summary info as specified by chars: (f)ailed, (E)rror, (s)kipped, (x)failed, (X)passed, (p)assed, (P)assed with output, (a)ll except passed (p/P), or (A)ll. (w)arnings are enabled by default (see --disable-warnings), 'N' can be used to reset the list. (default: 'fE').")
     args = parser.parse_args()
 
+    targets = []
     if args.ignore_skip_files:
         os.environ["TEST_MODELS_OPS_IGNORE_SKIP_FILES"] = "1"
 
@@ -17,6 +19,9 @@ def main():
         targets = ["models"]
     else:
         targets = glob.glob(f"models/*/test_{args.opname}_*.py")
+
+    if args.report:
+        targets.append("-r" + args.report)
 
     pytest.main(targets)
 
