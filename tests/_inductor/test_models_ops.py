9fbc1c5d0ca97cdc9a397f31f79d766554eb552e
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2025-12-02 18:39:58 +0000

    add more commandline options

--- tests/_inductor/test_models_ops.py
+++ tests/_inductor/test_models_ops.py
@@ -5,10 +5,18 @@ import pytest
 
 
 def main():
-    parser = argparse.ArgumentParser(description="driver for running test cases derived from models") 
+    parser = argparse.ArgumentParser(
+        description="driver for running test cases derived from models"
+    )
     parser.add_argument("--opname", help="torch operation name to be tested")
-    parser.add_argument("--ignore_skip_files", action="store_true", help="ignore model/skip_files.yaml")
-    parser.add_argument("--report", type=str, help="Show extra test summary info as specified by chars: (f)ailed, (E)rror, (s)kipped, (x)failed, (X)passed, (p)assed, (P)assed with output, (a)ll except passed (p/P), or (A)ll. (w)arnings are enabled by default (see --disable-warnings), 'N' can be used to reset the list. (default: 'fE').")
+    parser.add_argument(
+        "--ignore_skip_files", action="store_true", help="ignore model/skip_files.yaml"
+    )
+    parser.add_argument(
+        "--report",
+        type=str,
+        help="Show extra test summary info as specified by chars: (f)ailed, (E)rror, (s)kipped, (x)failed, (X)passed, (p)assed, (P)assed with output, (a)ll except passed (p/P), or (A)ll. (w)arnings are enabled by default (see --disable-warnings), 'N' can be used to reset the list. (default: 'fE').",
+    )
     args = parser.parse_args()
 
     targets = []
@@ -24,6 +32,7 @@ def main():
         targets.append("-r" + args.report)
 
     pytest.main(targets)
+
 
 if __name__ == "__main__":
     main()
