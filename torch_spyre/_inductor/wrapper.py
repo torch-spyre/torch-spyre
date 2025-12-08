0e01e118e81f01c24424df7a83e75ee74751af7a
David Grove <groved@us.ibm.com>
2025-12-08 23:54:35 +0000

    initial impl of codegen for spyre tensor allocation
    Signed-off-by: David Grove <groved@us.ibm.com>

--- torch_spyre/_inductor/wrapper.py
+++ torch_spyre/_inductor/wrapper.py
@@ -54,7 +54,7 @@ class SpyrePythonWrapperCodegen(PythonWrapperCodegen):
             """
                 from torch_spyre._inductor.runtime import ConstantArg, TensorArg, KernelSpec, UnimplementedOp
                 from torch_spyre._inductor.runtime.async_compile import SpyreAsyncCompile
-                from torch_spyre._C import SpyreTensorLayout, StickFormat
+                from torch_spyre._C import SpyreTensorLayout, StickFormat, spyre_empty
                 import subprocess
             """,
             strip=True,
@@ -75,15 +75,19 @@ class SpyrePythonWrapperCodegen(PythonWrapperCodegen):
         codegen_stride_tuple = self.codegen_python_shape_tuple(tuple(layout.stride))
 
         out = (
-            f"{name} = empty_strided("
+            f"{name} = spyre_empty("
             f"{codegen_allocation_shape_tuple}, "
             f"{codegen_stride_tuple}, "
-            f"device='{layout.device.type}', dtype={layout.dtype})"
+            f"{layout.dtype}, "
+            f"SpyreTensorLayout({layout.device_layout.device_size}, "
+            f"{layout.device_layout.device_strides}, "
+            f"{layout.device_layout.dim_map}, "
+            f"{layout.device_layout.num_stick_dims}, "
+            f"{layout.device_layout.format}))"
         )
         if codegen_shape_tuple != codegen_allocation_shape_tuple:
             out = out + f".as_strided({codegen_shape_tuple}, {codegen_stride_tuple})"
 
-        out = out + f" {self.comment} {layout.device_layout}"
         return out
 
 
--- torch_spyre/csrc/spyre_mem.cpp
+++ torch_spyre/csrc/spyre_mem.cpp
@@ -715,14 +715,11 @@ at::Tensor spyre_empty_strided(c10::IntArrayRef size, c10::IntArrayRef stride,
 
 at::Tensor spyre_empty_strided_layout(c10::IntArrayRef size,
                                       c10::IntArrayRef stride,
-                                      SpyreTensorLayout device_layout,
-                                      std::optional<c10::ScalarType> dtype_opt,
-                                      std::optional<c10::Layout> layout_opt,
-                                      std::optional<c10::Device> device_opt,
-                                      std::optional<bool> pin_memory_opt) {
+                                      c10::ScalarType dtype,
+                                      SpyreTensorLayout device_layout) {
   // TEMP: forward to empty_strided for now.
-  return spyre_empty_strided(size, stride, dtype_opt, layout_opt, device_opt,
-                             pin_memory_opt);
+  return spyre_empty_strided(size, stride, dtype, std::nullopt, std::nullopt,
+                             std::nullopt);
 }
 
 at::Tensor spyre_as_strided(const at::Tensor& self, c10::IntArrayRef size,
