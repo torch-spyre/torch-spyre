b2fae19666baa94a637fe7aa71fe100b53a44abf
Joshua Rosenkranz <jmrosenk@us.ibm.com>
2025-12-16 21:34:08 +0000

    moved implementation of SpyreGuardImpl to spyre_guard.cpp; removed extra registration; addressed PR comments
    Signed-off-by: Joshua Rosenkranz <jmrosenk@us.ibm.com>

--- torch_spyre/csrc/spyre_guard.cpp
+++ torch_spyre/csrc/spyre_guard.cpp
@@ -16,10 +16,53 @@
 
 #include "spyre_guard.h"
 
+#include <ATen/core/op_registration/adaption.h>
+
 namespace spyre {
 
-// LITERALINCLUDE START: SPYRE GUARD REGISTRATION
+c10::DeviceType SpyreGuardImpl::type() const {
+  return c10::DeviceType::PrivateUse1;
+}
+
+c10::Device SpyreGuardImpl::exchangeDevice(c10::Device d) const {
+  auto old = getDevice();
+  setDevice(d);
+  return old;
+}
+
+c10::Device SpyreGuardImpl::getDevice() const {
+  return {type(), tls_idx};
+}
+
+void SpyreGuardImpl::setDevice(c10::Device d) const {
+  TORCH_INTERNAL_ASSERT(d.type() == type());
+  // (optionally tell your runtime to switch)
+  tls_idx = d.index();
+}
+
+void SpyreGuardImpl::uncheckedSetDevice(c10::Device) const noexcept {}
+
+c10::DeviceIndex SpyreGuardImpl::deviceCount() const noexcept {
+  //  FIXME (tmhoangt) - return actual device count
+  return 1;
+}
+
+// Do Spyre have streams, override
+// getStream/exchangeStream/.../recordDataPtrOnStream
+c10::Stream SpyreGuardImpl::getStream(c10::Device device) const {
+  return c10::Stream(c10::Stream::Default::DEFAULT, device);
+}
+
+c10::Stream SpyreGuardImpl::exchangeStream(c10::Stream stream) const {
+  return stream;
+}
+
+void SpyreGuardImpl::recordDataPtrOnStream(const c10::DataPtr&,
+                                           const c10::Stream&) const {}
+
+thread_local c10::DeviceIndex SpyreGuardImpl::tls_idx = 0;
+
+// Registration (runs at DSO load — after you import your module)
 C10_REGISTER_GUARD_IMPL(PrivateUse1, SpyreGuardImpl);
-// LITERALINCLUDE END: SPYRE GUARD REGISTRATION
 
 }  // namespace spyre
--- torch_spyre/csrc/spyre_guard.h
+++ torch_spyre/csrc/spyre_guard.h
@@ -16,7 +16,6 @@
 
 #pragma once
 
-#include <ATen/core/op_registration/adaption.h>
 #include <torch/library.h>
 
 namespace spyre {
@@ -25,44 +24,25 @@ struct SpyreGuardImpl final : c10::impl::DeviceGuardImplInterface {
   static thread_local c10::DeviceIndex
       tls_idx;  // your TLS (or delegate to your runtime)
 
-  c10::DeviceType type() const override {
-    return c10::DeviceType::PrivateUse1;
-  }
-  c10::Device exchangeDevice(c10::Device d) const override {
-    auto old = getDevice();
-    setDevice(d);
-    return old;
-  }
+  c10::DeviceType type() const override;
 
-  c10::Device getDevice() const override {
-    return {type(), tls_idx};
-  }
-  void setDevice(c10::Device d) const override {
-    TORCH_INTERNAL_ASSERT(d.type() == type());
-    // (optionally tell your runtime to switch)
-    tls_idx = d.index();
-  }
-  void uncheckedSetDevice(c10::Device) const noexcept {}
+  c10::Device exchangeDevice(c10::Device d) const override;
 
-  c10::DeviceIndex deviceCount() const noexcept override {
-    //  FIXME (tmhoangt) - return actual device count
-    return 1;
-  }
+  c10::Device getDevice() const override;
+
+  void setDevice(c10::Device d) const override;
+
+  void uncheckedSetDevice(c10::Device) const noexcept;
+
+  c10::DeviceIndex deviceCount() const noexcept override;
 
   // Do Spyre have streams, override
   // getStream/exchangeStream/.../recordDataPtrOnStream
-  c10::Stream getStream(c10::Device device) const override {
-    return c10::Stream(c10::Stream::Default::DEFAULT, device);
-  }
-  c10::Stream exchangeStream(c10::Stream stream) const override {
-    return stream;
-  }
-  void recordDataPtrOnStream(const c10::DataPtr&, const c10::Stream&) const {}
+  c10::Stream getStream(c10::Device device) const override;
+
+  c10::Stream exchangeStream(c10::Stream stream) const override;
+
+  void recordDataPtrOnStream(const c10::DataPtr&, const c10::Stream&) const;
 };
 
-thread_local c10::DeviceIndex SpyreGuardImpl::tls_idx = 0;
-
-// Registration (runs at DSO load — after you import your module)
-C10_REGISTER_GUARD_IMPL(PrivateUse1, SpyreGuardImpl);
-
-};  // namespace spyre
+}  // namespace spyre
