549c72ef41cec0ebd5ea68cf3018e4638bf65f72
Chih-Chieh Yang <7364402+cyang49@users.noreply.github.com>
2026-02-19 18:45:48 +0000

    fix silu eager mode dispatch
    Signed-off-by: Chih-Chieh Yang <7364402+cyang49@users.noreply.github.com>
    Co-authored-by: Antoni Viros i Martin <aviros@ibm.com>

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -212,6 +212,14 @@ class TestOps(TestCase):
         y = torch.relu(x_spyre).to("cpu")
         torch.testing.assert_close(y, torch.relu(x), rtol=self.rtol, atol=self.atol)
 
+    def test_silu(self):
+        x = torch.tensor([1, -2, 3], dtype=self.dtype)
+        x_spyre = x.to("spyre")
+        y = torch.nn.functional.silu(x_spyre).to("cpu")
+        torch.testing.assert_close(
+            y, torch.nn.functional.silu(x), rtol=self.rtol, atol=self.atol
+        )
+
     def test_exp(self):
         x = torch.tensor([-10, -1, 0, 1, 10], dtype=self.dtype)
         x_spyre = x.to("spyre")
