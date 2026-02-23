927aedee48a3ca7edfef5df312050e30bc02d95a
Chih-Chieh Yang <7364402+cyang49@users.noreply.github.com>
2026-02-23 18:59:48 +0000

    fix mish eager mode recursion error
    Signed-off-by: Chih-Chieh Yang <7364402+cyang49@users.noreply.github.com>

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -229,6 +229,14 @@ class TestOps(TestCase):
             y, torch.nn.functional.silu(x), rtol=self.rtol, atol=self.atol
         )
 
+    def test_mish(self):
+        x = torch.tensor([1, -2, 3], dtype=self.dtype)
+        x_spyre = x.to("spyre")
+        y = torch.nn.functional.mish(x_spyre).to("cpu")
+        torch.testing.assert_close(
+            y, torch.nn.functional.mish(x), rtol=self.rtol, atol=self.atol
+        )
+
     def test_exp(self):
         x = torch.tensor([-10, -1, 0, 1, 10], dtype=self.dtype)
         x_spyre = x.to("spyre")
