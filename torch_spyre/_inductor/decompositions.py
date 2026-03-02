2cd23d7c3f2416a2fd2fabeddf1a5c06deb72d0a
Thomas Ortner <boh@zurich.ibm.com>
2026-03-02 23:02:32 +0000

    WIP
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- torch_spyre/_inductor/decompositions.py
+++ torch_spyre/_inductor/decompositions.py
@@ -14,7 +14,7 @@
 
 from contextlib import contextmanager
 
-from typing import Optional, Union, Callable, TypeVar
+from typing import Optional, Union, Sequence, Callable, TypeVar
 from typing_extensions import ParamSpec
 
 import torch
@@ -372,7 +372,7 @@ def spyre_rms_norm(
     input: torch.Tensor,
     normalized_shape: list[int],
     weight: Optional[torch.Tensor] = None,
-    eps: Optional[float] = None,
+    eps: Optional[float] = 1e-5,
 ) -> torch.Tensor:
     if input.device.type != "spyre" or len(normalized_shape) != 1:
         raise Unsupported(
@@ -394,6 +394,48 @@ def spyre_rms_norm(
         output = output * weight
     return output
 
+# TODO (imaihal): Inductor applies constant folding to torch.full, which allocates
+# a one-element Spyre tensor. This currently fails because Spyre does not handle
+# single-element tensors well.
+# Ref: https://github.com/pytorch/pytorch/blob/v2.9.1/torch/_inductor/fx_passes/joint_graph.py#L324-L335
+#
+# To avoid constant folding, we introduce a custom op `spyre::full` that runs
+# torch.full on CPU and copies the result to Spyre. Remove this workaround once
+# Spyre supports one-element tensors.
+@register_spyre_decomposition([torch.ops.aten.full])
+def full_decomp(
+    size: list[Union[int, torch.SymInt]],
+    fill_value: torch.types.Number,
+    dtype: Optional[torch.dtype] = None,
+    layout: Optional[torch.layout] = None,
+    device: Optional[torch.device] = None,
+    pin_memory: Optional[bool] = None,
+) -> torch.Tensor:
+    assert layout in (torch.strided, None), f"doesn't support layout={layout}"
+    assert not pin_memory, f"doesn't support pin_memory={pin_memory}"
+    return torch.ops.spyre.full(size, fill_value, device, dtype=dtype)
+
+
+"""
+Hook torch.nn.functional.layer_norm to select spyre optimized version where applicable
+"""
+orig_layer_norm = torch.nn.functional.layer_norm
+
+
+def spyre_layer_norm(
+    input: torch.Tensor,
+    normalized_shape: Sequence[int],
+    weight: Optional[torch.Tensor] = None,
+    bias: Optional[torch.Tensor] = None,
+    eps: float = 1e-5,
+) -> torch.Tensor:
+    if input.device.type == "spyre" and len(normalized_shape) == 1:
+        return torch.ops.spyre.layer_norm(input, normalized_shape, weight, bias, eps)
+    else:
+        return orig_layer_norm(input, normalized_shape, weight, bias, eps)
+
+
+torch.nn.functional.layer_norm = spyre_layer_norm
 
 orig_gelu = torch.nn.functional.gelu
 
@@ -408,7 +450,7 @@ def spyre_gelu(
         return orig_gelu(input, approximate=approximate)
 
 
-torch.nn.functional.gelu = spyre_gelu
+# torch.nn.functional.gelu = spyre_gelu
 
 
 orig_softplus = torch.nn.functional.softplus
@@ -423,7 +465,7 @@ def spyre_softplus(
         return orig_softplus(input, beta, threshold)
 
 
-torch.nn.functional.softplus = spyre_softplus
+# torch.nn.functional.softplus = spyre_softplus
 
 
 @register_spyre_decomposition([torch.ops.aten.gt.Tensor, torch.ops.aten.gt.Tensor_out])
