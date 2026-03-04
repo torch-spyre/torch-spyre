05229994bebd02756e9b8b734c9122127b7ed893
Thomas Ortner <boh@zurich.ibm.com>
2026-03-04 23:52:31 +0000

    Lint
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- torch_spyre/_inductor/decompositions.py
+++ torch_spyre/_inductor/decompositions.py
@@ -398,6 +398,7 @@ def lt_decomp(
     out_ne = torch.ne(input, other).to(dtype=torch.float16)
     return torch.mul(out_le, out_ne, out=out).to(dtype=torch.bool)
 
+
 @register_spyre_decomposition([torch.ops.aten.logical_not])
 def logical_not_decomp(input: torch.Tensor) -> torch.Tensor:
     # Currently falling back to torch.zeros_like for dtypes other than bool
