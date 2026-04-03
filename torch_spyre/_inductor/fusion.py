b9ab0dc4d63374e8acb0cc8a68e0e5ba3d248a63
David Grove <groved@us.ibm.com>
2026-04-03 21:27:12 +0000

    Enable fusion subject to current constraints of SDSC Bundles
    Signed-off-by: David Grove <groved@us.ibm.com>

--- torch_spyre/_inductor/fusion.py
+++ torch_spyre/_inductor/fusion.py
@@ -12,18 +12,58 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from torch._inductor.scheduler import BaseSchedulerNode, FusedSchedulerNode
+from torch._inductor.scheduler import (
+    BaseSchedulerNode,
+    FusedSchedulerNode,
+    SchedulerNode,
+)
 
 from torch_spyre._inductor.logging_utils import _get_env_bool
 
-_FUSION_ENABLED = _get_env_bool("SPYRE_INDUCTOR_ENABLE_FUSION")
+# Temporary hook to easily disable
+_FUSION_ENABLED = _get_env_bool("SPYRE_INDUCTOR_ENABLE_FUSION", True)
 
 
 def spyre_fuse_nodes(nodes: list[BaseSchedulerNode]) -> list[BaseSchedulerNode]:
     """
-    If fusion is enabled, put all nodes into a single SpyreKernel
+    Fuse nodes together to form kernels without changing their order.
+    Each kernel will be compiled into a single SuperDSC Bundle.
+    Fusion is limited by the following constraints.
+     1. We only want to fuse SchedulerNodes (ie, nodes that generate OpSpecs).
+     2. A SDSC Bundle can refer to at most 6 unique tensors (until we complete  https://github.com/torch-spyre/torch-spyre/issues/827).
     """
     if not _FUSION_ENABLED or len(nodes) == 0:
         return nodes
 
-    return [FusedSchedulerNode(nodes[0].scheduler, nodes)]
+    fused_nodes: list[BaseSchedulerNode] = []
+    cur_nodes: list[SchedulerNode] = []
+    cur_tensors: set[str] = set()
+    for n in nodes:
+        if isinstance(n, SchedulerNode):
+            n_tensors = {dep.name for dep in n.read_writes.reads_and_writes()}
+            cur_tensors |= n_tensors
+            if len(cur_tensors) < 7:
+                # Ok to put in the current bundle
+                cur_nodes.append(n)
+            else:
+                # Would be too many tensors in the Bundle; start a new one.
+                fused_nodes.append(
+                    FusedSchedulerNode(cur_nodes[0].scheduler, cur_nodes)
+                )
+                cur_nodes = [n]
+                cur_tensors = n_tensors
+
+        else:
+            # Other node types (eg Fallback nodes) force a bundle boundary.
+            if len(cur_nodes) > 0:
+                fused_nodes.append(
+                    FusedSchedulerNode(cur_nodes[0].scheduler, cur_nodes)
+                )
+            fused_nodes.append(n)
+            cur_nodes = []
+            cur_tensors = set()
+
+    if len(cur_nodes) > 0:
+        fused_nodes.append(FusedSchedulerNode(cur_nodes[0].scheduler, cur_nodes))
+
+    return fused_nodes
