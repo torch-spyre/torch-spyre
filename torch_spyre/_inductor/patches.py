0872707849640d20074ec21050381b53d04965fc
David Grove <groved@us.ibm.com>
2026-04-10 22:47:05 +0000

    move stickify to a post-lowering pre-scheduling pass
    Signed-off-by: David Grove <groved@us.ibm.com>

--- torch_spyre/_inductor/patches.py
+++ torch_spyre/_inductor/patches.py
@@ -20,6 +20,8 @@ from torch._inductor.utils import InputType
 from torch._inductor.virtualized import V
 from typing import Callable, Optional
 
+from .constants import DEVICE_NAME
+
 
 @contextmanager
 def spyre_data_types():
