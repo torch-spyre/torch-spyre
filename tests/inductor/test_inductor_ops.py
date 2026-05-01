11c971aac57ba8d0f8f2e92319606856f76c2929
kcirred <16872435+kcirred@users.noreply.github.com>
2026-05-01 18:06:42 -0400

    autocast in torch-spyre (#1617)
    * autocast in torch-spyre
    
    Signed-off-by: kcirred <16872435+kcirred@users.noreply.github.com>
    
    * [autocast] tests for autocast
    
    Signed-off-by: kcirred <16872435+kcirred@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: kcirred <16872435+kcirred@users.noreply.github.com>
    Co-authored-by: Antoni Viros <aviros@ibm.com>
    Signed-off-by: Urvashi Klair <Urvashi.Klair@ibm.com>

--- tests/inductor/test_inductor_ops.py
+++ tests/inductor/test_inductor_ops.py
@@ -157,6 +157,26 @@ class TestOps(unittest.TestCase, metaclass=ParameterizedTestMeta):
                 rand_type="xavier",
             ),
         },
+        ("test_mm_autocast", "test_mm_autocast_cpu"): {
+            "param_sets": {
+                "fp32_enabled": (
+                    True,
+                    cached_randn((4, 4), dtype=torch.float32),
+                    cached_randn((4, 4), differentiation=1, dtype=torch.float32),
+                ),
+                "f16_enabled": (
+                    True,
+                    cached_randn((4, 4), dtype=torch.float16),
+                    cached_randn((4, 4), differentiation=1, dtype=torch.float16),
+                ),
+                "f16_disabled": (
+                    False,
+                    cached_randn((4, 4), differentiation=2, dtype=torch.float16),
+                    cached_randn((4, 4), differentiation=3, dtype=torch.float16),
+                ),
+            },
+            "expect_fail": ["fp32_enabled"],
+        },
         ("test_einsum", "test_mm_relaxed"): {
             "ops_dict": {
                 "einsum": lambda a, b: torch.einsum("mk, kn -> mn", a, b),
@@ -2033,6 +2053,13 @@ class TestOps(unittest.TestCase, metaclass=ParameterizedTestMeta):
             compare_with_cpu(op, a, b, atol=0.1, rtol=0.1)
         else:  # single stick, no need to relax
             compare_with_cpu(op, a, b)
+
+    def test_mm_autocast_cpu(self, enabled, a, b):
+        def fn(a, b):
+            with torch.autocast(device_type="spyre", enabled=enabled):
+                return a @ b
+
+        compare_with_cpu(fn, a, b)
 
     def test_binary_op_cpu(self, op, x, y):
         # Eager mode support varies by op:
