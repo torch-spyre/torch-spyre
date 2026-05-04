a63ac6159ff58d41b6c7bf31d5c661bef4753c28
Jonel Poueriet Santana <jonel.poueriet@ibm.com>
2026-05-04 15:27:24 -0400

    Correcting argument passing for job_plan.cpp RuntimeOperation::create function substitutions
    Signed-off-by: Jonel Poueriet Santana <jonel.poueriet@ibm.com>

--- torch_spyre/csrc/job_plan.cpp
+++ torch_spyre/csrc/job_plan.cpp
@@ -30,14 +30,14 @@ namespace spyre {
 std::unique_ptr<flex::RuntimeOperation> JobPlanStepH2D::construct(
     LaunchContext&) const {
   auto op = flex::RuntimeOperationH2D::create(host_address_,
-                                                        &device_address_);
+                                                        device_address_);
   op->setPipelineBarrier(pipeline_barrier_);
   return op;
 }
 
 std::unique_ptr<flex::RuntimeOperation> JobPlanStepD2H::construct(
     LaunchContext&) const {
-  auto op = flex::RuntimeOperationD2H::create(&device_address_,
+  auto op = flex::RuntimeOperationD2H::create(device_address_,
                                                         host_address_);
   op->setPipelineBarrier(pipeline_barrier_);
   return op;
@@ -45,7 +45,7 @@ std::unique_ptr<flex::RuntimeOperation> JobPlanStepD2H::construct(
 
 std::unique_ptr<flex::RuntimeOperation> JobPlanStepCompute::construct(
     LaunchContext&) const {
-  auto op = flex::RuntimeOperationCompute::create(&binary_address_);
+  auto op = flex::RuntimeOperationCompute::create(binary_address_);
   op->setPipelineBarrier(pipeline_barrier_);
   return op;
 }
