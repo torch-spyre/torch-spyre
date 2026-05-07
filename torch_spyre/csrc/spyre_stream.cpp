e3fc4acb8a5228fa62bc2d8e8066c19b7a04a69d
Jonel Poueriet Santana <jonel.poueriet@ibm.com>
2026-05-06 20:56:26 -0400

    Correct reference RuntimeOperationCompute::create call
    Signed-off-by: Jonel Poueriet Santana <jonel.poueriet@ibm.com>

--- torch_spyre/csrc/spyre_stream.cpp
+++ torch_spyre/csrc/spyre_stream.cpp
@@ -237,7 +237,7 @@ void SpyreStream::executeProgramAsync(
   // Program
   auto* ctx = static_cast<SharedOwnerCtx*>(arts.device_alloc.get_context());
   auto compute_op = flex::RuntimeOperationCompute::create(
-      &ctx->composite_addr, std::move(tensor_allocs), arts.bundle_mlir_path);
+      ctx->composite_addr, std::move(tensor_allocs), arts.bundle_mlir_path);
 
   // Get the flex runtime stream handle
   flex::RuntimeStream* flex_stream = getRuntimeHandle();
