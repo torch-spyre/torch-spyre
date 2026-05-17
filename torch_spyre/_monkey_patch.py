5e8f3bd21775cab1bcac7522a376d969cb710405
wuyangfan <1102042793@qq.com>
2026-05-18 01:24:10 +0800

    fix: accept size keyword in spyre empty
    Signed-off-by: wuyangfan <1102042793@qq.com>

--- torch_spyre/_monkey_patch.py
+++ torch_spyre/_monkey_patch.py
@@ -122,6 +122,7 @@ def _patch_tensor_for_spyre():
 
     def spyre_empty(
         *args,
+        size=None,
         device_layout=None,
         out=None,
         dtype=None,
@@ -131,6 +132,14 @@ def _patch_tensor_for_spyre():
         pin_memory=False,
         memory_format=torch.contiguous_format,
     ):
+        if size is not None:
+            if args:
+                raise TypeError(
+                    "torch.empty() received both positional size arguments "
+                    "and a 'size' keyword"
+                )
+            args = (size,)
+
         if (
             device_layout is None
         ):  # use original implementation if no layout is provided
