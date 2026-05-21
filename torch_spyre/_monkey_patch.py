5840fdd032fc63c827da1affb2845c3b2d89712e
Avery Blanchard <avery.blanchard@ibm.com>
2026-05-21 15:20:57 -0400

    Remove monkey patch changes for now
    Signed-off-by: Avery Blanchard <avery.blanchard@ibm.com>

--- torch_spyre/_monkey_patch.py
+++ torch_spyre/_monkey_patch.py
@@ -196,7 +196,8 @@ def _patch_tensor_for_spyre():
                 x.device.type != DEVICE_NAME
                 or x.device_tensor_layout() == expected_layout
             ),
-            [f"SpyreTensorLayout({guard.name}) == {expected_layout}"]
+            [f"SpyreTensorLayout({guard.name}) == {expected_layout}"],
+            guard.user_stack,
         )
 
     GuardBuilder.TENSOR_MATCH = _spyre_TENSOR_MATCH
