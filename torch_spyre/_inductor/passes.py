e553c0828195c1b0892f7d5c3e66229bba6020ee
Olivier Tardieu <tardieu@us.ibm.com>
2026-05-21 19:04:33 +0000

    Implement working set reduction hints
    Signed-off-by: Olivier Tardieu <tardieu@us.ibm.com>

--- torch_spyre/_inductor/passes.py
+++ torch_spyre/_inductor/passes.py
@@ -36,6 +36,10 @@ from .temp_passes import (
     convert_constant_with_graph_node,
 )
 from . import config
+from .propagate_hints import (
+    collect_spyre_hints,
+    recover_spyre_hints,
+)
 from .propagate_layouts import (
     propagate_mutation_layouts,
     propagate_spyre_tensor_layouts,
@@ -111,7 +115,7 @@ class CustomPrePasses(CustomGraphPass):
     """
     The list of custom passes to run
     """
-    passes: List[Callable[[torch.fx.graph.Graph], None]] = []
+    passes: List[Callable[[torch.fx.graph.Graph], None]] = [collect_spyre_hints]
 
     def __call__(self, graph: torch.fx.graph.Graph) -> None:
         for p in CustomPrePasses.passes:
@@ -133,6 +137,7 @@ class CustomPostPasses(CustomGraphPass):
     The list of custom passes to run
     """
     passes: List[Callable[[torch.fx.graph.Graph], None]] = [
+        recover_spyre_hints,
         convert_constant_with_graph_node,
         mm_to_bmm_pass.apply,
         bmm_unflatten_pass.apply,
