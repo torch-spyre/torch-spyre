af5e200db144b3a77a19e29f1a9828221c2321ae
Isuru Fernando <ifernando@openteams.com>
2026-06-23 01:06:27 +0530

    Lower aten.silu to silu OpFunc (#2780)
    * Detect silu patterns and lower them to silu OpFunc
    
    Signed-off-by: Isuru Fernando <ifernando@openteams.com>
    
    * remove pattern matching and use aten op
    
    Signed-off-by: Isuru Fernando <ifernando@openteams.com>
    
    * add silu tests
    
    Signed-off-by: Isuru Fernando <ifernando@openteams.com>
    
    * Fix tests
    
    Signed-off-by: Isuru Fernando <ifernando@openteams.com>
    
    * fix signature
    
    Signed-off-by: Isuru Fernando <ifernando@openteams.com>
    
    ---------
    
    Signed-off-by: Isuru Fernando <ifernando@openteams.com>
    Co-authored-by: David Grove <dgrove-oss@users.noreply.github.com>
    Co-authored-by: Avery Blanchard <74270185+avery-blanchard@users.noreply.github.com>
    Signed-off-by: Uma Devi <umamadev@in.ibm.com>

--- torch_spyre/_inductor/spyre_kernel.py
+++ torch_spyre/_inductor/spyre_kernel.py
@@ -364,6 +364,10 @@ class SpyreOpFuncs:
         return PointwiseOp("realdiv", [a, b])
 
     @staticmethod
+    def silu(a):
+        return PointwiseOp("silu", [a])
+
+    @staticmethod
     def where(x, y, z):
         return PointwiseOp("where3", [x, y, z])
 
