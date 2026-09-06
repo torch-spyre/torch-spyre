713c50be1c562727591438c19f42737eee85f607
Haruki Imai <imaihal@jp.ibm.com>
2026-09-06 02:40:50 +0000

    Clean up bcast test
    Signed-off-by: Haruki Imai <imaihal@jp.ibm.com>

--- tests/inductor/test_inductor_ops.py
+++ tests/inductor/test_inductor_ops.py
@@ -907,32 +907,21 @@ class TestOps(unittest.TestCase, metaclass=ParameterizedTestMeta):
                     torch.randint(-100, 100, (67, 71, 256), dtype=torch.int64),
                     torch.randint(-100, 100, (67, 71, 256), dtype=torch.int64),
                 ),
+                "1d_2d_bcast": (
+                    torch.randint(-100, 100, (32,), dtype=torch.int64),
+                    torch.randint(-100, 100, (32, 32), dtype=torch.int64),
+                ),
+                "1d_2d_1_bcast": (
+                    torch.randint(-100, 100, (32, 1), dtype=torch.int64),
+                    torch.randint(-100, 100, (32, 32), dtype=torch.int64),
+                ),
+                "1d_3d": (
+                    torch.randint(-100, 100, (256,), dtype=torch.int64),
+                    torch.randint(-100, 100, (67, 71, 256), dtype=torch.int64),
+                ),
             },
+            "expect_fail": ["1d_2d_1_bcast"],
         },
-        # (
-        #    "test_pointwise_binary_op_int64_broadcast",
-        #    "test_binary_op",
-        # ): {
-        #    "ops_dict": POINTWISE_BINARY_OPS_INT64_DICT,
-        #    "param_sets": {
-        #        "1d_2d": (
-        #            torch.randint(-100, 100, (32,), dtype=torch.int64),
-        #            torch.randint(-100, 100, (32, 32), dtype=torch.int64),
-        #        ),
-        #        "1d_2d_1": (
-        #            torch.randint(-100, 100, (32, 1), dtype=torch.int64),
-        #            torch.randint(-100, 100, (32, 32), dtype=torch.int64),
-        #        ),
-        #        "1d_2d_1_fp32": (
-        #            torch.randint(-100, 100, (32, 1), dtype=torch.float32),
-        #            torch.randint(-100, 100, (32, 32), dtype=torch.float32),
-        #        ),
-        #        "1d_3d": (
-        #            torch.randint(-100, 100, (256,), dtype=torch.int64),
-        #            torch.randint(-100, 100, (67, 71, 256), dtype=torch.int64),
-        #        ),
-        #    },
-        # },
         ("test_add_broadcast", "test_add_broadcast"): {
             "param_sets": make_param_dict(
                 [
