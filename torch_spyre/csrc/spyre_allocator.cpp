2b3dd6e2dde08f689e63080db9858f186a8b3145
Luke-Gutierrez <luke.gutierrez@ibm.com>
2026-09-11 15:07:51 -0400

    update compositeAddressToDmva use
    Signed-off-by: Luke-Gutierrez <luke.gutierrez@ibm.com>

--- torch_spyre/csrc/spyre_allocator.cpp
+++ torch_spyre/csrc/spyre_allocator.cpp
@@ -219,7 +219,7 @@ void SpyreAllocator::copy_data(void* dest, const void* src,
 
 uint64_t SpyreAllocator::compositeAddressToDmva(
     const flex::CompositeAddress& addr) const {
-  return getFlexAllocator()->compositeAddressToDmva(addr);
+  return flex::compositeAddressToDmva(addr);
 }
 
 void SpyreAllocator::memoryPressureCallback(
